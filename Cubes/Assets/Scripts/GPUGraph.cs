565b6b503ac2723c339a5290581ebf9da743c3b3
chicamhao <bluevampire0103@gmail.com>
2024-08-14 01:23:28 +0700

    draw the graph using compute shaders

---
+++ Cubes/Assets/Scripts/GPUGraph.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Drawing;
+using Unity.VisualScripting;
+using UnityEngine;
+using static MathUtility;
+
+public sealed class GPUGraph : MonoBehaviour
+{
+    enum TransitionMode { Cycle, Random };
+    [SerializeField] TransitionMode _mode;
+    [SerializeField] GraphType _currentGraph;
+
+    [SerializeField, Range(10, 200)] int _resolution;
+    [SerializeField, Min(0f)] float _functionDuration = 1f;
+    [SerializeField, Min(0f)] float _transitionDuration = 1f;
+
+    [SerializeField] ComputeShader _computerShader;
+    [SerializeField] Material _material;
+    [SerializeField] Mesh _mesh;
+
+    static readonly int _positionsId = Shader.PropertyToID("_Positions");
+    static readonly int _resolutionId = Shader.PropertyToID("_Resolution");
+    static readonly int _stepId = Shader.PropertyToID("_Step");
+    static readonly int _timeId = Shader.PropertyToID("_Time");
+
+    GraphType _transitionGraph;
+    float _duration;
+    bool _transiting;
+
+    ComputeBuffer _positionsBuffer;
+
+    private void OnEnable()
+    {
+        _positionsBuffer = new ComputeBuffer(_resolution * _resolution, 3 * 4);
+    }
+
+    private void OnDisable()
+    {
+        _positionsBuffer.Release();
+        _positionsBuffer = null;
+    }
+
+    private void Update()
+    {
+        _duration += Time.deltaTime;
+        if (_transiting)
+        {
+            if (_duration >= _transitionDuration)
+            {
+                _duration -= _transitionDuration;
+                _transiting = false;
+            }
+        }
+        else if (_duration >= _functionDuration)
+        {
+            _duration -= _functionDuration;
+            _transiting = true;
+
+            _transitionGraph = _currentGraph;
+            _currentGraph = _mode switch
+            {
+                TransitionMode.Cycle => GetNextGraphFrom(_currentGraph),
+                TransitionMode.Random => GetRandomFrom(_currentGraph),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        UpdateGraphOnGPU();
+    }
+
+    private void UpdateGraphOnGPU()
+    {
+        var step = 2f / _resolution;
+        _computerShader.SetInt(_resolutionId, _resolution);
+        _computerShader.SetFloat(_stepId, step);
+        _computerShader.SetFloat(_timeId, Time.time);
+
+        _computerShader.SetBuffer(0, _positionsId, _positionsBuffer);
+
+        var groups = Mathf.CeilToInt(_resolution / 8f);
+        _computerShader.Dispatch(0, groups, groups, 1);
+
+        _material.SetBuffer(_positionsId, _positionsBuffer);
+        _material.SetFloat(_stepId, step);
+
+        //  points should remain inside a cube with size 2,
+        // but points have a size as well, half of which could poke outside the bounds in all directions.
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + step));
+        Graphics.DrawMeshInstancedProcedural(_mesh, 0, _material, bounds, _positionsBuffer.count);
+    }
+}
---
+++ Cubes/Assets/Scripts/MathUtility.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using static UnityEngine.Mathf;
+
+public static class MathUtility
+{
+    public delegate Vector3 GraphFunc(float u, float v, float t);
+    public enum GraphType
+    {
+        Wave,
+        MorphingWave,
+        Ripple,
+        TwistedSphere,
+        Torus
+    }
+    static readonly GraphFunc[] _funcs = { Wave, MorphingWave, Ripple, TwistedSphere, Torus };
+
+    public static GraphFunc GetGraphFunc(GraphType type) => _funcs[(int)type];
+
+    public static GraphType GetNextGraphFrom(GraphType current)
+    {
+        return (GraphType)Repeat((float)current + 1, _funcs.Length - 1);
+    }
+
+    public static GraphType GetRandomFrom(GraphType current)
+    {
+        var choice = current;
+        while (choice == current)
+        {
+            choice = (GraphType)Random.Range(0, _funcs.Length);
+        }
+        return choice;
+    }
+
+    public static Vector3 Morph(float u, float v, float t, GraphFunc from, GraphFunc to, float progress)
+    {
+        return Vector3.LerpUnclamped(from(u, v, t), to(u, v, t), SmoothStep(0f, 1f, progress));
+    }
+
+    private static Vector3 Wave(float u, float v, float t)
+    {
+        return new Vector3(u, Sin(PI * (u + v + t)), v);
+    }
+
+    private static Vector3 MorphingWave(float u, float v, float t)
+    {
+        var y = Sin((u + 0.5f * t) * PI);
+        y += 0.5f * Sin(2f * PI * (v + t));
+        y += Sin(PI * (u + v + 0.25f * t));
+        y *= 1f / 2.5f;
+        return new Vector3(u, y, v);
+    }
+
+    private static Vector3 Ripple(float u, float v, float t)
+    {
+        var d = Sqrt(u * u + v * v);
+        var y = Sin(PI * (4f * d - t)); // b: period, t: horizontal shift
+        y /= 1f + 10f * d; // decreases with distance because a ripple doesn't have a fixed amplitude
+        return new Vector3(u, y, v);
+    }
+
+    private static Vector3 TwistedSphere(float u, float v, float t)
+    {
+        var r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+        var s = r * Cos(0.5f * PI * v);
+        return new Vector3(
+            s * Sin(PI * u),
+            r * Sin(0.5f * PI * v),
+            s * Cos(PI * u)
+            );
+    }
+
+    private static Vector3 Torus(float u, float v, float t)
+    {
+        var r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        var r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        var s = r1 + r2 * Cos(PI * v);
+        return new Vector3(
+            s * Sin(PI * u),
+            r2 * Sin(PI * u),
+            s * Cos(PI * u)
+            );
+    }
+}
+
